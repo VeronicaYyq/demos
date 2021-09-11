598246601e2bf202c190e788f1d55c44348cc90b
yyq <yuanyq@253.com>
2021-09-11 15:30:38 +0800

    netty心跳断连重试机制

---
+++ netty/src/main/java/demo07/client/ClientHandler.java
@@ -0,0 +1,53 @@
+package demo07.client;
+
+import demo07.CustomHeartbeatHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+
+public class ClientHandler extends CustomHeartbeatHandler {
+  /*  public ClientHandler() {
+        super("client");
+    }
+
+    @Override
+    protected void handleData(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) {
+        byte[] data = new byte[byteBuf.readableBytes() - 5];
+        byteBuf.skipBytes(5);
+        byteBuf.readBytes(data);
+        String content = new String(data);
+        System.out.println(name + " get content: " + content);
+    }
+
+    @Override
+    protected void handleAllIdle(ChannelHandlerContext ctx) {
+        super.handleAllIdle(ctx);
+        sendPingMsg(ctx);
+    }*/
+
+    private Client client;
+    public ClientHandler(Client client) {
+        super("client");
+        this.client = client;
+    }
+
+    @Override
+    protected void handleData(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) {
+        byte[] data = new byte[byteBuf.readableBytes() - 5];
+        byteBuf.skipBytes(5);
+        byteBuf.readBytes(data);
+        String content = new String(data);
+        System.out.println(name + " get content: " + content);
+    }
+
+    @Override
+    protected void handleAllIdle(ChannelHandlerContext ctx) {
+        super.handleAllIdle(ctx);
+        sendPingMsg(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        super.channelInactive(ctx);
+        client.doConnect();
+    }
+}
---
+++ netty/src/main/java/demo07/server/ServerHandler.java
@@ -0,0 +1,30 @@
+package demo07.server;
+
+import demo07.CustomHeartbeatHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+
+public class ServerHandler extends CustomHeartbeatHandler {
+    public ServerHandler() {
+        super("服务端");
+    }
+
+    @Override
+    protected void handleData(ChannelHandlerContext channelHandlerContext, ByteBuf buf) {
+        byte[] data = new byte[buf.readableBytes() - 5];
+        ByteBuf responseBuf = Unpooled.copiedBuffer(buf);
+        buf.skipBytes(5);
+        buf.readBytes(data);
+        String content = new String(data);
+        System.out.println(name + " 接收的 content: " + content);
+        channelHandlerContext.write(responseBuf);
+    }
+
+    @Override
+    protected void handleReaderIdle(ChannelHandlerContext ctx) {
+        super.handleReaderIdle(ctx);
+        System.err.println("---client " + ctx.channel().remoteAddress().toString() + " reader timeout, close it---");
+        ctx.close();
+    }
+}
